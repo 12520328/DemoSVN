79564ae5ee3067d61b86a100ae47ce07a07eaddb
Võ Hoài Phương <12520328@gm.uit.edu.vn>
2015-05-23 20:51:06 +0700

    thằng chó ngu

--- DemoSVN/Form1.cs
+++ DemoSVN/Form1.cs
@@ -27,5 +27,10 @@ namespace DemoSVN
         {
             MessageBox.Show("Form1");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ghadkakjga;lk");
+        }
     }
 }
